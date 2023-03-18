2deb561b80d8360adf275f74c54b15ee5a7fb040
karkaned <ermek.maks10@gmail.com>
2023-03-18 20:20:54 +0600

    add buttons

--- handlers/fsm_anketa.py
+++ handlers/fsm_anketa.py
@@ -2,6 +2,7 @@ from aiogram import  types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from keyboards import client_kb
 
 
 class FSMAdmin(StatesGroup):
@@ -9,12 +10,13 @@ class FSMAdmin(StatesGroup):
     age = State()
     group = State()
     direction = State()
+    submit = State()
 
 
 async def fsm_start(message: types.Message):
     if message.chat.type == "private":
         await FSMAdmin.name.set()
-        await message.answer("Enter your name: ")
+        await message.answer("Enter your name: ", reply_markup=client_kb.cancel)
     else:
         await message.answer("Write to the group!")
 
@@ -48,7 +50,7 @@ async def load_group(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['group'] = message.text
     await FSMAdmin.next()
-    await message.answer("What direction are you studying?: ")
+    await message.answer("What direction are you studying?: ", reply_markup=client_kb.direction_markup)
 
 
 async def load_direction(message: types.Message, state: FSMContext):
@@ -57,13 +59,35 @@ async def load_direction(message: types.Message, state: FSMContext):
         await message.answer(f"{data['name']} {data['age']} {data['group']} {data['direction']}\n"
                              f"@{data['username']}")
     await FSMAdmin.next()
-    await message.answer("Is everything right?")
+    await message.answer("Is everything right?", reply_markup=client_kb.submit_markup)
+
+
+async def submit(message: types.Message, state: FSMContext):
+    if message.text == "yes":
+        # Save user data to database here
+        await state.finish()
+    elif message.text == "no":
+        await state.finish()
+    else:
+        await message.answer("Please answer 'yes' or 'no'")
+
+
+async def cansel_reg(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state:
+        await state.finish()
+        await message.answer("canceled✔️️")
 
 
 def register_handlers_fsm_anketa(dp: Dispatcher):
+    dp.register_message_handler(cansel_reg, state='*', commands=['cancel'])
+    dp.register_message_handler(cansel_reg,
+                                Text(equals="cancel", ignore_case=True), state='*')
+
     dp.register_message_handler(fsm_start, commands=['reg'])
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_age, state=FSMAdmin.age)
     dp.register_message_handler(load_group, state=FSMAdmin.group)
     dp.register_message_handler(load_direction, state=FSMAdmin.direction)
+    dp.register_message_handler(submit, state=FSMAdmin.submit)
 
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -19,3 +19,32 @@ start_markup.add(
     share_location,
     share_contact
 )
+
+cancel_button = KeyboardButton("cancel"),
+
+cancel = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).add(
+    cancel_button
+)
+
+direction_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).add(
+    KeyboardButton("backend"),
+    KeyboardButton("frontend"),
+    KeyboardButton("ios"),
+    KeyboardButton("android"),
+    cancel_button
+)
+
+submit_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).add(
+    KeyboardButton("yes"),
+    KeyboardButton("no"),
+    cancel_button
+)
